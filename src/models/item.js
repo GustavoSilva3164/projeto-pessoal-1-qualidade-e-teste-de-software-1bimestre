0e382cbf2d730b78ec28077a41ab52142e6ce32d
GustavoSilva3164 <gustavosilva2008br@gmail.com>
2026-03-29 16:24:45 -0300

    updt

--- src/models/item.js
+++ src/models/item.js
@@ -1,16 +1,16 @@
-class Item {
-  constructor(nome, preco) {
-    if (!nome) {
-      throw new Error('Nome do item é obrigatório');
-    }
-
-    if (preco < 0) {
-      throw new Error('Preço inválido');
-    }
-
-    this.nome = nome;
-    this.preco = preco;
+function Item(nome, preco) {
+  if (!nome) {
+    throw new Error('Nome do item é obrigatório')
   }
+
+  if (preco < 0) {
+    throw new Error('Preço inválido')
+  }
+
+  return {
+    nome,
+    preco
+  };
 }
 
-module.exports = Item;
+module.exports = Item
